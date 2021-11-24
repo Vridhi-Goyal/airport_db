1abd49df26cc1b294967ece3e29ea90add43fa29
Vridhi Goyal <61738982+Vridhi-Goyal@users.noreply.github.com>
2021-11-25 01:18:13 +0530

    update ddl.sql
    changed atc priveledge

--- ddl.sql
+++ ddl.sql
@@ -5,8 +5,7 @@ create user agency with password 'agency';
 grant insert, select on flight to agency;
 
 create user atc with password 'atc';
-grant select on runway, runs_on to atc;
-grant insert, update on flight to atc;
+grant insert, update on flight,runway, runs_on to atc;
 
 create user scheduler with password 'scheduler';
 grant select, insert, update on flight to scheduler; 
